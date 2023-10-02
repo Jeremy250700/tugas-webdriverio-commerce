425bfe5237a260f1e9df97d62c5eefff2d924bda
Jeremy250700 <jeremysaputra25@gmail.com>
2023-10-02 12:37:27 +0700

    update afterEach FT_002_detail_product_page.js

--- testing/FT_002_detail_product_page.test.js
+++ testing/FT_002_detail_product_page.test.js
@@ -11,6 +11,7 @@ describe('FT_002_Detail_Product_Page',function(){
     /**@type {CatalogPage} */let catalogPage
     /**@type {DetailProductPage} */let detailProductPage
     /**@type {CartPage} */let cartPage
+    let notMove = false
 
     before(async function(){
         driver = await setupDriver()
@@ -28,6 +29,7 @@ describe('FT_002_Detail_Product_Page',function(){
             await driver.pause(1000)
             const productTitle = await detailProductPage.title.getText()
             expect(productName).to.equal(productTitle)
+            notMove = true
         })
         
     })
@@ -38,6 +40,7 @@ describe('FT_002_Detail_Product_Page',function(){
             const title= await detailProductPage.modalReviewTitle.getText()
             expect(title).to.equal('Thank you for submitting your review!')
             await detailProductPage.closeModal()
+            notMove = true
         })
         
     })
@@ -48,6 +51,7 @@ describe('FT_002_Detail_Product_Page',function(){
             const title= await detailProductPage.modalReviewTitle.getText()
             expect(title).to.equal('Thank you for submitting your review!')
             await detailProductPage.closeModal()
+            notMove = true
         })
         
     })
@@ -58,6 +62,7 @@ describe('FT_002_Detail_Product_Page',function(){
             const title= await detailProductPage.modalReviewTitle.getText()
             expect(title).to.equal('Thank you for submitting your review!')
             await detailProductPage.closeModal()
+            notMove = true
         })
         
     })
@@ -70,10 +75,8 @@ describe('FT_002_Detail_Product_Page',function(){
             await cartPage.cartButton.click()
             const blueCart = await cartPage.colorBlue.getAttribute('content-desc')
             expect(blueDetailProduct).to.equal(blueCart)
-
             await driver.pause(500)
             await cartPage.removeItemButton.click()
-
         })
     })
     describe('DPC_006 Mencoba mengubah warna product menjadi abu abu', function(){
@@ -85,7 +88,6 @@ describe('FT_002_Detail_Product_Page',function(){
             await cartPage.cartButton.click()
             const grayCart = await cartPage.colorGray.getAttribute('content-desc')
             expect(grayDetailProduct).to.equal(grayCart)
-            
             await driver.pause(500)
             await cartPage.removeItemButton.click()
 
@@ -100,7 +102,6 @@ describe('FT_002_Detail_Product_Page',function(){
             await cartPage.cartButton.click()
             const redCart = await cartPage.colorRed.getAttribute('content-desc')
             expect(redDetailProduct).to.equal(redCart)
-            
             await driver.pause(500)
             await cartPage.removeItemButton.click()
 
@@ -108,26 +109,31 @@ describe('FT_002_Detail_Product_Page',function(){
     })
     describe('DPC_008 Mencoba menambah quantity product', function(){
         it('Quantity product bertambah', async function(){
-            await detailProductPage.increaseQtyProduct()
+            await detailProductPage.scrollDown()
+            await detailProductPage.increaseQtyButton.click()
             await driver.pause(500)
             const qty = await detailProductPage.qty.getText()
             expect(qty).to.equal('2')
+            await detailProductPage.decreaseQtyButton.click()
+            notMove = true
         })
         
     })
     describe('DPC_009 Mencoba mengurang quantity product', function(){
         it('Quantity product berkurang', async function(){
-            await detailProductPage.decreaseQtyProduct()
+            await detailProductPage.decreaseQtyButton.click()
             await driver.pause(500)
             const qty = await detailProductPage.qty.getText()
             expect(qty).to.equal('0')
+            await detailProductPage.increaseQtyButton.click()
+            notMove = true
         })
         
     })
     describe('DPC_010 Mencoba memasukan product ke cart dengan quantity 0',function(){
         it('item tidak muncul pada halaman cart',async function(){
-            await detailProductPage.decreaseQtyProduct()
-            await detailProductPage.addToCart()
+            await detailProductPage.decreaseQtyButton.click()
+            await detailProductPage.addToCartButton.click()
             await cartPage.cartButton.click()
             await driver.pause(500)
             const title = await cartPage.cartTitle.getText()
@@ -136,10 +142,12 @@ describe('FT_002_Detail_Product_Page',function(){
     })
     describe('DPC_011 Mencoba memasukan product ke cart dengan quantity 1',function(){
         it('badge pada button cart menampilkan angka 1',async function(){
-            await detailProductPage.addToCart()
+            await detailProductPage.scrollDown()
+            await detailProductPage.addToCartButton.click()
             const badge = await cartPage.cartBadge.getText()
             expect(badge).to.equal('1')
            await detailProductPage.scrollUp()
+           notMove = true
         })
         it('item muncul pada halaman cart',async function(){
             const detailProductName = await detailProductPage.title.getText()
@@ -147,30 +155,40 @@ describe('FT_002_Detail_Product_Page',function(){
             await driver.pause(500)
             const title = await cartPage.productName.getText()
             expect(title).to.equal(detailProductName)
+            await driver.pause(500)
             await cartPage.removeItemButton.click()
         })
     })
     describe('DPC_012 Mencoba memasukan product ke cart dengan quantity 2',function(){
         it('badge pada button cart menampilkan angka 2',async function(){
-            await detailProductPage.increaseQtyProduct()
-            await detailProductPage.addToCart()
+            await detailProductPage.scrollDown()
+            await detailProductPage.increaseQtyButton.click()
+            await detailProductPage.addToCartButton.click()
             const badge = await cartPage.cartBadge.getText()
             expect(badge).to.equal('2')
             await detailProductPage.scrollUp()
+            notMove = true
         })
         it('harga item dikali 2',async function(){
             await cartPage.cartButton.click()
             await driver.pause(500)
             const totalPirce = await cartPage.totalPrice.getText()
             expect(totalPirce).to.equal('$59.98')
+            await driver.pause(500)
+            await cartPage.removeItemButton.click()
         })
-    })
+    }) 
     afterEach(async function(){
-        await catalogPage.burgerButton.click()
-        await driver.pause(1000)
-        await catalogPage.catalogButton.click()
-        await catalogPage.productName1.click()
-        await driver.pause(1000)
+        if(notMove == true){
+            await driver.pause(1000)
+            notMove = false
+        }else{
+            await driver.pause(500)
+            await cartPage.shoppingButton.click()
+            await driver.pause(500)
+            await catalogPage.productName1.click()
+            await driver.pause(1000)
+        }
     })
     after(async function(){
         await driver.deleteSession()
